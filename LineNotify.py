74a289dbcf95944a81ada80012ed3b9c2032a251
Yoshida <16814385+yoshida121@users.noreply.github.com>
2019-01-22 23:31:20 +0900

    File Rename.

---
+++ LineNotify.py
@@ -0,0 +1,15 @@
+
+import requests
+
+class Notify():
+    def __init__(self, token, token_path=None):
+        if token_path is not None:
+            with open(token_path) as token_file:
+                token = token_file.read()
+        self.token = token
+        self.api = 'https://notify-api.line.me/api/notify'
+
+    def notify(self, message):
+        payload = {"message": message}
+        headers = {'Authorization': 'Bearer ' + self.token}
+        requests.post(self.api, data=payload, headers=headers)
